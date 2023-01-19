b2af42b2eed6f47011ea612852db65cac07b98bb
captain-eddie <edward.guobadia@gmail.com>
2023-01-19 16:21:03 -0500

    Setup program window
    Setup program window

--- circle_dots.py
+++ circle_dots.py
@@ -16,6 +16,15 @@ def main():
     #   draw circle
     pg.draw.circle(window, constants.WHITE, (50, 50), 10.0)
 
+    #   main window loop
+    while True:
+        pg.display.update()
+
+        #   window event handler
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+
 
 if __name__ == "__main__":
     main()
