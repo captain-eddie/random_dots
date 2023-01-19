33c99a5080a5c9f3db857432914818652b22b3ee
captain-eddie <edward.guobadia@gmail.com>
2023-01-19 16:17:41 -0500

    Made constants file
    Made constants file

--- circle_dots.py
+++ circle_dots.py
@@ -5,6 +5,7 @@ Edward Abel-Guobadia
 '''
 
 import pygame as pg
+import constants
 
 def main():
     pg.init()
@@ -13,7 +14,7 @@ def main():
     window = pg.display.set_mode((100, 100))
 
     #   draw circle
-    pg.draw.circle(window, (255))
+    pg.draw.circle(window, constants.WHITE, (50, 50), 10.0)
 
 
 if __name__ == "__main__":
