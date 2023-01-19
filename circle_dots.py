5f720ea8ef62eb3058248cc8703f4c388bbf7dcc
captain-eddie <edward.guobadia@gmail.com>
2023-01-19 16:13:17 -0500

    First commit
    First commit

---
+++ circle_dots.py
@@ -0,0 +1,20 @@
+
+'''
+Edward Abel-Guobadia
+1-19-2023
+'''
+
+import pygame as pg
+
+def main():
+    pg.init()
+
+    #   program window
+    window = pg.display.set_mode((100, 100))
+
+    #   draw circle
+    pg.draw.circle(window, (255))
+
+
+if __name__ == "__main__":
+    main()
